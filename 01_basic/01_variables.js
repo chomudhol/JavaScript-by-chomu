98801abb5b086d3747f8a461761809ebdc4b73ae
chomudhol <vishnushuthar024@gmail.com>
2024-07-31 02:13:26 +0000

    datatyoe

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1234
+let accountEmail ="raju3232@gamil.com"
+var accountPassword = "352643"
+accountCity = "gaya"
+let accountstate;
+
+// accountId = 2 //not allowed
+console.log(accountId);
+
+
+accountEmail ="raju3232@gamil.com"
+accountPassword = "352643"
+accountCity = "sorapur"    //hota he pr use nhi kre 
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountstate]);
+
+/*
+prefer not to use var
+becouse of issue in block scope and function scope   '{}'
+*/
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,17 @@
+// alert(3 + 3)
+let name  = "erry"
+let age  = 35
+let isLoggedIn  = false
+    //    premitive DT
+//number = 2 to power 53
+//bigint 
+// string => ""
+// Boolean => true/false
+// null => standalone value he
+// undefine =. abhi value asine nhi hui he
+// symbol => unique 
+
+//object
+console.log(typeof (age))
+console.log(typeof (null))      // OT=> object
+console.log(typeof (undefined))  //OT=>undefined

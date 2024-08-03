2723ffd039ac887deca231ae916e2fbfa9a89c81
Vishnu.S.101 <vishnushuthar024@gmail.com>
2024-08-03 08:29:26 +0000

    datatype-summery

---
+++ 01_basic/datatypes-summmery.js
@@ -0,0 +1,32 @@
+// # primitive :
+                // 1. String
+                // 2. Number
+                // 3. Boolean
+                // 4. null
+                // 5. undefined
+                // 6. Symbol
+                // 7. BigInt
+
+// #Reference (Non Primitive) :
+                // 1. Array
+                // 2. Objects
+                // 3. function
+
+//   1.Array => 
+    //  const heros = ["joy","machoo","husay"]                    
+//   2. obj.=>
+                // let myobj = {
+                //           name : "joy",
+                //           age  : 34,
+                //            }  
+//   let Null = null  
+
+//  3. func. =>
+    //  const myFunction = function(){
+    //                console.lo("Hello Word")
+    //                                     }                          
+
+console.log(typeof(heros))
+console.log(typeof(myobj))
+console.log(typeof(myFunction))
+console.log(typeof(Nulll))
